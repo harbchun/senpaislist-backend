44922112552f4423bac3735b619a23e1b055ba54
MarioH95 <harb.mario@gmail.com>
2021-05-24 18:59:31 -0300

    Add initial migrations

--- migrations/000001_create_anime_table.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS animes(
-   anime_id serial PRIMARY KEY,
-   title VARCHAR (50) UNIQUE NOT NULL,
-   title_jp VARCHAR (50) NOT NULL,
-   anime_description VARCHAR (300) UNIQUE NOT NULL
-);
---
+++ migrations/000010_create_user_anime_bookmarks.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS user_anime_bookmarks(
+   user_id uuid NOT NULL,
+   anime_id uuid NOT NULL,
+   bookmark_type VARCHAR NOT NULL,
+   PRIMARY KEY (user_id, anime_id),
+   CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+   CONSTRAINT fk_anime_id FOREIGN KEY (anime_id) REFERENCES animes (id) ON DELETE CASCADE
+);
