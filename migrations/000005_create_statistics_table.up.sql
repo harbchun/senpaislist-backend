48185b0631ad3cf4a7ced7ecd1e8b184d7c22dc3
MarioH95 <harb.mario@gmail.com>
2021-05-30 01:01:40 -0300

    Remove not null restriction from stats table

--- migrations/000005_create_statistics_table.up.sql
+++ migrations/000005_create_statistics_table.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS statistics(
    anime_id uuid NOT NULL,
-   score int NOT NULL,
-   scored_by int NOT NULL,
-   rank int NOT NULL,
-   popularity INT NOT NULL,
-   favorites int NOT NULL,
+   score int,
+   scored_by int,
+   rank int,
+   popularity INT,
+   favorites int,
    rating INT,
    CONSTRAINT fk_anime_id FOREIGN KEY (anime_id) REFERENCES animes (id)
 );
