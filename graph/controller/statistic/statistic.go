f0ad7c7056602ceae1bf1d9d61275e01529f312c
MarioH95 <harb.mario@gmail.com>
2021-05-30 19:58:59 -0300

    Fix model sources

--- graph/controller/statistic/statistic.go
+++ graph/controller/statistic/statistic.go
@@ -5,14 +5,14 @@ import (
 	"fmt"
 	"log"
 
-	models "github.com/harrisonwjs/senpaislist-backend/graph/model"
+	"github.com/harrisonwjs/senpaislist-backend/graph/model"
 )
 
 type Statistic struct {
 	DB *sql.DB
 }
 
-func (a *Statistic) GetStatistic(id string) models.Statistic {
+func (a *Statistic) GetStatistic(id string) model.Statistic {
 	query, err := a.DB.Prepare("SELECT * FROM statistics WHERE anime_id=$1")
 	if err != nil {
 		log.Fatal(err)
@@ -23,7 +23,7 @@ func (a *Statistic) GetStatistic(id string) models.Statistic {
 		log.Fatal(err)
 	}
 
-	var statistic models.Statistic
+	var statistic model.Statistic
 
 	if res.Next() {
 		err = res.Scan(&statistic.AnimeID, &statistic.Score, &statistic.ScoredBy, &statistic.Rank, &statistic.Popularity, &statistic.Favorites, &statistic.Rating)
