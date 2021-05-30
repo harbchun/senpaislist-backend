1d5235a5b0c7cf30fd26392963cf9448ca8ecf20
MarioH95 <harb.mario@gmail.com>
2021-05-29 22:39:47 -0300

    Add anime controller to resolver to be able to inject to resolvers

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,8 +1,12 @@
 package graph
 
+import "github.com/harrisonwjs/senpaislist-backend/graph/controller/anime"
+
+//go:generate go run github.com/99designs/gqlgen
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
+	AnimeController anime.Anime
 }
