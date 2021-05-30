61df2370ddf39953a4c694ddce6721717f3b4271
MarioH95 <harb.mario@gmail.com>
2021-05-30 19:58:43 -0300

    Add fetching of multiple animes

--- graph/controller/anime/anime.go
+++ graph/controller/anime/anime.go
@@ -2,7 +2,6 @@ package anime
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 
 	"github.com/harrisonwjs/senpaislist-backend/graph/model"
@@ -36,6 +35,29 @@ func (a *Anime) GetAnime(id string) model.Anime {
 	return anime
 }
 
-func (a *Anime) GetAnimes() {
-	fmt.Println("working animes")
+func (a *Anime) GetAnimes(id *string, title *string) []*model.Anime {
+	query, err := a.DB.Prepare("SELECT * FROM animes WHERE (id=$1 OR $1 IS NULL) AND (title=$2 OR $2 IS NULL)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := query.Query(id, title)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var animes []*model.Anime
+
+	for res.Next() {
+		var anime model.Anime
+		err = res.Scan(&anime.ID, &anime.Title, &anime.TitleJp, &anime.Tid, &anime.Source, &anime.Studio, &anime.Summary, &anime.ImageURL)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		animes = append(animes, &anime)
+	}
+
+	return animes
 }
