35562552ff464418f92ce1beb40c6be064e39ef3
MarioH95 <harb.mario@gmail.com>
2021-05-30 19:54:48 -0300

    Add genres controller

---
+++ graph/controller/genre/genre.go
@@ -0,0 +1,39 @@
+package genre
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/harrisonwjs/senpaislist-backend/graph/model"
+)
+
+type Genre struct {
+	DB *sql.DB
+}
+
+func (a *Genre) GetGenres() []*model.Genre {
+	query, err := a.DB.Prepare("SELECT * FROM genres")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := query.Query()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var genres []*model.Genre
+
+	for res.Next() {
+		var genre model.Genre
+		err = res.Scan(&genre.ID, &genre.Genre)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		genres = append(genres, &genre)
+	}
+
+	return genres
+}
