eb7f0f0a008104d5306d36f688634097fc8f44ff
MarioH95 <harb.mario@gmail.com>
2021-06-06 00:18:38 -0300

    Redesign functions due to filter/order by potentially being nullable and passing it to a function can throw errors

--- graph/controller/anime/anime.go
+++ graph/controller/anime/anime.go
@@ -3,6 +3,8 @@ package anime
 import (
 	"database/sql"
 	"log"
+	"strconv"
+	"strings"
 
 	"github.com/harrisonwjs/senpaislist-backend/graph/model"
 )
@@ -11,21 +13,7 @@ type Anime struct {
 	DB *sql.DB
 }
 
-func queryBuilder(f *model.AnimeFilterInput, j *model.AnimeSortInput) (string, string, string, string) {
-	query, joinMap := filterFunc(f)
-
-	joinOrderMap, order, group := sortFunc(j, joinMap)
-
-	var join string
-
-	for _, element := range joinOrderMap {
-		join += element
-	}
-
-	return query, join, order, group
-}
-
-func sortFunc(f *model.AnimeSortInput, j map[string]string) (map[string]string, string, string) {
+func sortFunc(f *model.AnimeSortInput, j string) (string, string, string) {
 	if f != nil {
 		return animeSortInput(f, j)
 	}
@@ -33,7 +21,7 @@ func sortFunc(f *model.AnimeSortInput, j map[string]string) (map[string]string, 
 	return j, "", ""
 }
 
-func animeSortInput(f *model.AnimeSortInput, j map[string]string) (map[string]string, string, string) {
+func animeSortInput(f *model.AnimeSortInput, j string) (string, string, string) {
 	if len(f.Statistics) > 0 {
 		for _, sort := range f.Statistics {
 			if sort != nil {
@@ -45,11 +33,13 @@ func animeSortInput(f *model.AnimeSortInput, j map[string]string) (map[string]st
 	return j, "", ""
 }
 
-func statisticsSortInput(f *model.StatisticsSortInput, j map[string]string) (map[string]string, string, string) {
+func statisticsSortInput(f *model.StatisticsSortInput, j string) (string, string, string) {
 	s := ""
 	g := ""
 	if f.Popularity != nil {
-		j["statistics"] = " INNER JOIN statistics ON animes.id = statistics.anime_id "
+		if !strings.Contains(j, "INNER JOIN statistics ON animes.id = statistics.anime_id") {
+			j += " INNER JOIN statistics ON animes.id = statistics.anime_id "
+		}
 		if *f.Popularity == "asc" {
 			s = " ORDER BY statistics.popularity ASC"
 		} else {
@@ -61,43 +51,50 @@ func statisticsSortInput(f *model.StatisticsSortInput, j map[string]string) (map
 	return j, s, g
 }
 
-func airingInformationsSortInput(f *model.AiringInformationsSortInput, j map[string]string) (map[string]string, string) {
+func airingInformationsSortInput(f *model.AiringInformationsSortInput, j string) (string, string) {
 	s := ""
 	return j, s
 }
 
-func filterFunc(f *model.AnimeFilterInput) (string, map[string]string) {
-	j := make(map[string]string)
+func filterFunc(f *model.AnimeFilterInput) (string, string) {
+	j := ""
 
-	query, joinMap := animeFilterInput(f, j)
+	query, join := animeFilterInput(f, j)
+	if len(query) > 0 {
+		query = " WHERE " + query[4:]
+	}
 
-	return " WHERE " + query, joinMap
+	return query, join
 }
 
-func animeFilterInput(f *model.AnimeFilterInput, j map[string]string) (string, map[string]string) {
+func animeFilterInput(f *model.AnimeFilterInput, j string) (string, string) {
 	s := ""
-
 	if len(f.Or) > 0 {
-		s += "("
+		s += " AND "
+		s += " ( "
 		for i, filter := range f.Or {
-			if i != 0 {
-				s += " OR "
-			}
 			query, join := animeFilterInput(filter, j)
+			query = query[4:]
 			s += query
 			j = join
+			//remove first and of the sequence
+			if i != len(f.Or)-1 {
+				s += " OR "
+			}
 		}
 		s += ")"
 	}
 	if len(f.And) > 0 {
+		s += " AND "
 		s += "("
 		for i, filter := range f.And {
-			if i != 0 {
-				s += " AND "
-			}
 			query, join := animeFilterInput(filter, j)
+			query = query[4:]
 			s += query
 			j = join
+			if i != len(f.And)-1 {
+				s += " AND "
+			}
 		}
 		s += ")"
 	}
@@ -105,31 +102,54 @@ func animeFilterInput(f *model.AnimeFilterInput, j map[string]string) (string, m
 		for _, filter := range f.AnimeGenres {
 			query, join := animeGenresFilterInput(filter, j)
 			s += query
-			j = join
+			j += join
 		}
+	}
+	if len(f.AiringInformations) > 0 {
+		for _, filter := range f.AiringInformations {
+			query, join := animeAiringInformationsInput(filter, j)
+			s += query
+			j += join
+		}
+	}
+	if f.Title != nil {
+		s += " AND animes.title = " + "'" + *f.Title.Eq + "'"
 	}
 
 	return s, j
 }
 
-func animeGenresFilterInput(f *model.AnimesGenresFilterInput, j map[string]string) (string, map[string]string) {
+func animeGenresFilterInput(f *model.AnimesGenresFilterInput, j string) (string, string) {
 	s := ""
+	lj := ""
 	if f.Genre != nil {
 		if f.Genre.Eq != nil {
-			s += " animes_genres.genre = " + "'" + *f.Genre.Eq + "'"
-			j["genres"] = " INNER JOIN animes_genres ON animes.id = animes_genres.anime_id "
-		}
-	}
-	if f.Genre != nil && f.AnimeID != nil {
-		s += " AND "
-	}
-	if f.AnimeID != nil {
-		if f.AnimeID.Eq != nil {
-			s += " anime.id = " + "'" + *f.AnimeID.Eq + "'"
+			s += " AND animes_genres.genre = " + "'" + *f.Genre.Eq + "'"
+			if !strings.Contains(j, "INNER JOIN animes_genres ON animes.id = animes_genres.anime_id") {
+				lj = " INNER JOIN animes_genres ON animes.id = animes_genres.anime_id "
+			}
 		}
 	}
 
-	return s, j
+	return s, lj
+}
+
+func animeAiringInformationsInput(f *model.AiringInformationsFilterInput, j string) (string, string) {
+	s := ""
+	lj := ""
+	if f.Season != nil {
+		s += " AND airing_informations.season = " + "'" + *f.Season.Eq + "'"
+		if !strings.Contains(j, "INNER JOIN airing_informations ON animes.id = airing_informations.anime_id") {
+			lj = " INNER JOIN airing_informations ON animes.id = airing_informations.anime_id "
+		}
+	}
+	if f.Year != nil {
+		s += " AND airing_informations.year = " + strconv.Itoa(*f.Year.Eq)
+		if !strings.Contains(j, "INNER JOIN airing_informations ON animes.id = airing_informations.anime_id") {
+			lj = " INNER JOIN airing_informations ON animes.id = airing_informations.anime_id "
+		}
+	}
+	return s, lj
 }
 
 func (a *Anime) GetAnime(id string) model.Anime {
@@ -157,13 +177,22 @@ func (a *Anime) GetAnime(id string) model.Anime {
 }
 
 func (a *Anime) GetAnimes(filter *model.AnimeFilterInput, orderBy *model.AnimeSortInput) []*model.Anime {
-	s, j, b, g := queryBuilder(filter, orderBy)
-	query, err := a.DB.Prepare("SELECT animes.* FROM animes " + j + s + " GROUP BY animes.id " + g + b)
+	where, join, group, order := "", "", "", ""
+	if filter != nil {
+		where, join = filterFunc(filter)
+	}
+	if orderBy != nil {
+		join, order, group = sortFunc(orderBy, join)
+	}
+
+	log.Println("SELECT animes.* FROM animes " + join + where + " GROUP BY animes.id " + group + order)
+	query, err := a.DB.Prepare("SELECT animes.* FROM animes " + join + where + " GROUP BY animes.id " + group + order)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	res, err := query.Query()
+	defer res.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
